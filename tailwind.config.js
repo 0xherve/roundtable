6c9f242b5f575faa442b6aebcf3e02eebcc68459
imenarh <h.rwigema@alustudent.com>
2025-07-19 17:29:09 +0000

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./**/*.{html,js,ts,tsx}"], // Include .ts and .tsx files
+module.exports = {
+  content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
       colors: {
