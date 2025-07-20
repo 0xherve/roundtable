9cf3e8e3d64728c76a33359778c0f6057e5b767b
imenarh <h.rwigema@alustudent.com>
2025-07-20 06:42:47 +0000

    looks better than before but bad

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // For static export (if needed)
-  output: 'export',
-  trailingSlash: true,
+  // output: 'export',
+  // trailingSlash: true,
   images: {
     unoptimized: true
   }
