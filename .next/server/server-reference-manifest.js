93ff5a28ceef869afbfe8f399f008b5578df432e
imenarh <h.rwigema@alustudent.com>
2025-07-20 08:35:33 +0000

    real site

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K6GMxxN+lSP9JQrlPyqmCvZvxJO2M/3z6031BGfZM3I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
